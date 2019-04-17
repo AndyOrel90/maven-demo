84467de913b51cb008681fd7f138c1f6ea69acd4
Mehrab <mehrab.rahman@revature.com>
2019-04-17 15:36:05 -0500

    Interfaces and abstract classes

---
+++ src/main/java/com/revature/MoscowMule.java
@@ -0,0 +1,17 @@
+package com.revature;
+
+public class MoscowMule extends Mule {
+
+	@Override
+	public void drink() {
+		System.out.println("Da!");
+
+	}
+
+	@Override
+	public void mix() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/main/java/com/revature/TexasMule.java
+++ src/main/java/com/revature/TexasMule.java
@@ -3,40 +3,22 @@
  */
 package com.revature;
 
-import java.util.Arrays;
-
 /**
  * @author Mehrab
  *
  */
-public class TexasMule {
-	private String ingredients[];
-
-	public String[] getIngredients() {
-		return ingredients;
+public class TexasMule extends Mule{
+	/* (non-Javadoc)
+	 * @see com.revature.Drinkable#drink()
+	 */
+	@Override
+	public void drink() {
+		System.out.println("Yeehaw!");
 	}
 
-	public void setIngredients(String ...ingredients) {
-		this.ingredients = ingredients;
-	}
-
-	public static void main(String ...args) {
-		if (args.length > 0) {
-			TexasMule firstDrink = new TexasMule();	
-			firstDrink.ingredients = args;
-			Arrays.sort(firstDrink.ingredients);
-			System.out.println(Arrays.toString(firstDrink.ingredients));
-		}	
+	@Override
+	public void mix() {
+		// TODO Auto-generated method stub
 		
-		String[] newIngredients = {"Pig", "Lion"};
-		TexasMule secondDrink = new TexasMule();
-		secondDrink.setIngredients("Pig", "Lion", "Bear");
-		for (int i = 0; i < secondDrink.getIngredients().length; i++) {
-			System.out.println(secondDrink.getIngredients()[i]);
-		}
-		
-		for(String s : secondDrink.getIngredients()) {
-			System.out.println(s);
-		}
 	}
 }
