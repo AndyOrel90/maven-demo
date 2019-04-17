ba51051af29835fbe30bfcfd8e93e8bde277ed59
Mehrab <mehrab.rahman@revature.com>
2019-04-17 11:22:46 -0500

    Arrays or something

--- src/main/java/com/revature/TexasMule.java
+++ src/main/java/com/revature/TexasMule.java
@@ -3,27 +3,40 @@
  */
 package com.revature;
 
+import java.util.Arrays;
+
 /**
  * @author Mehrab
  *
  */
 public class TexasMule {
+	private String ingredients[];
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		String ingredient1 = "Horse";
-		String ingredient2 = "Donkey";
-		
-		TexasMule firstDrink = new TexasMule();
-		String finishedFirstDrink = firstDrink.add(ingredient1, ingredient2);
-		
-		System.out.println(finishedFirstDrink);
-	}
-	
-	public String add(String a, String b) {
-		return a + b;
+	public String[] getIngredients() {
+		return ingredients;
 	}
 
+	public void setIngredients(String ...ingredients) {
+		this.ingredients = ingredients;
+	}
+
+	public static void main(String ...args) {
+		if (args.length > 0) {
+			TexasMule firstDrink = new TexasMule();	
+			firstDrink.ingredients = args;
+			Arrays.sort(firstDrink.ingredients);
+			System.out.println(Arrays.toString(firstDrink.ingredients));
+		}	
+		
+		String[] newIngredients = {"Pig", "Lion"};
+		TexasMule secondDrink = new TexasMule();
+		secondDrink.setIngredients("Pig", "Lion", "Bear");
+		for (int i = 0; i < secondDrink.getIngredients().length; i++) {
+			System.out.println(secondDrink.getIngredients()[i]);
+		}
+		
+		for(String s : secondDrink.getIngredients()) {
+			System.out.println(s);
+		}
+	}
 }
